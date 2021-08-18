3f1650258b304bc4d532d793b707823e5317f601
Sreeraj T <sreeraj2408@gmail.com>
2021-08-18 15:34:25 +0530

    Add new API to find factorial of a number

--- src/main/java/com/example/demo/controller/NumberOperationsController.java
+++ src/main/java/com/example/demo/controller/NumberOperationsController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.IntStream;
+
 @RestController
 @RequestMapping("/demo")
 public class NumberOperationsController {
@@ -13,5 +15,11 @@ public class NumberOperationsController {
     @GetMapping("/sum/{number1}/{number2}")
     public ResponseEntity sumOfNumbers(@PathVariable("number1") int number1, @PathVariable("number2") int number2) {
         return ResponseEntity.ok(number1 + number2);
+    }
+
+    @GetMapping("/factorial/{number}")
+    public ResponseEntity factorialOfNumber(@PathVariable("number") int number) {
+        int factorial = IntStream.rangeClosed(1, number).reduce(1, (acc, val) -> acc * val);
+        return ResponseEntity.ok(factorial) ;
     }
 }
--- src/test/java/com/example/demo/controller/NumberOperationsControllerTest.java
+++ src/test/java/com/example/demo/controller/NumberOperationsControllerTest.java
@@ -1,30 +1,46 @@
 package com.example.demo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.http.HttpStatus;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.HttpStatus.OK;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class NumberOperationsControllerTest {
 
-    NumberOperationsController controller;
+    private NumberOperationsController controller;
+    private ObjectMapper mapper;
 
     @BeforeAll
     public void setUp() {
         this.controller = new NumberOperationsController();
+        this.mapper = new ObjectMapper();
     }
 
-    @Test
-    public void return_sum_of_two_numbers() {
-        int number1 = 10, number2 = 20;
+    @ParameterizedTest
+    @CsvSource({"10, 20, 30", "3, 2, 5"})
+    public void return_sum_of_two_numbers(int n1, int n2, int expected) throws JsonProcessingException {
+        ResponseEntity responseEntity = controller.sumOfNumbers(n1, n2);
 
-        ResponseEntity responseEntity = controller.sumOfNumbers(number1, number2);
-
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isEqualTo(30);
+        assertEquals(OK, responseEntity.getStatusCode());
+        Integer actual = mapper.readValue(responseEntity.getBody().toString(), Integer.class);
+        assertEquals(expected, actual);
     }
+
+    @ParameterizedTest
+    @CsvSource({"5, 120", "3, 6", "4, 24"})
+    public void return_factorial_of_number(int number, int expected) throws JsonProcessingException {
+        ResponseEntity responseEntity = controller.factorialOfNumber(number);
+
+        assertEquals(OK, responseEntity.getStatusCode());
+        Integer actual = mapper.readValue(responseEntity.getBody().toString(), Integer.class);
+        assertEquals(expected, actual);
+    }
+
 }
